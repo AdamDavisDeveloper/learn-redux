eacfbaaaa7866d8785701bd9663ddf52495b8832
Adam Davis <adavisdeveloper@gmail.com>
2021-04-23 01:33:55 -0400

    add dispatch / counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div>
-      <h1>Counter</h1>
-      <button>Increment</button>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
     </div>
   );
 }
